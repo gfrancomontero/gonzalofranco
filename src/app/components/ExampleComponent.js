3edc3f4785186f48f557be1ebe786a3b36a8f593
Gonzalo Franco <gfrancomontero@gmail.com>
2024-06-13 13:38:50 +0200

    portfolio boilerplate

---
+++ src/app/components/ExampleComponent.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './example.module.scss'
+
+const ExampleComponent = () => {
+  return (
+    <div className="bg-blue-500 text-white p-4 rounded-lg p-">
+      <p className="text-lg">Hello, this is a Tailwind CSS styled component!</p>
+    </div>
+  )
+}
+
+export default ExampleComponent
--- src/components/icons/logo.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const IconLogo = () => (
-  <title>Logo</title>
-);
-
-export default IconLogo;
