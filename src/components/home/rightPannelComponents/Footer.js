ae4f40173dbd58f0d056af300c37072ca6e85c03
Gonzalo Franco <gfrancomontero@gmail.com>
2024-06-14 19:46:28 +0200

    excape character

--- src/components/home/rightPannelComponents/Footer.js
+++ src/components/home/rightPannelComponents/Footer.js
@@ -6,7 +6,7 @@ export default function Footer() {
     <div className="w-full flex flex-col justify-center items-center align-center text-white mb-18">
       <Image className="mb-8" width={80} alt="NextUI hero Image" src={`/me.png`} />
       <p className="mb-8 max-w-[500px] min-w-[400px] text-slate-400 text-lg text-justify w-full">
-        Even though I&amp;m not actively seeking new opportunities right now, my inbox is always open. If you have a question or just want to say hello, I'll do my best to respond!
+        Even though I&amp;m not actively seeking new opportunities right now, my inbox is always open. If you have a question or just want to say hello, I&apos;ll do my best to respond!
       </p>
 
       <a href="mailto:hey@gonzalofranco.com" target="_blank">
