bf79cdac72aac0013eaa506850db7defdf8038d0
Gonzalo Franco <gfrancomontero@gmail.com>
2024-06-15 01:12:49 +0200

    fixed some more style issues

--- src/components/home/rightPannelComponents/Footer.js
+++ src/components/home/rightPannelComponents/Footer.js
@@ -4,9 +4,9 @@ import {Code} from "@nextui-org/react";
 export default function Footer() {
   return (
     <div className="w-full flex flex-col justify-center items-center align-center text-white mb-18">
-      <Image className="mb-8" width={80} alt="NextUI hero Image" src={`/me.png`} />
-      <p className="mb-8 max-w-[500px] min-w-[400px] text-slate-400 text-lg text-justify w-full">
-        Even though I&amp;m not actively seeking new opportunities right now, my inbox is always open. If you have a question or just want to say hello, I&apos;ll do my best to respond!
+      <Image className="mb-8 w-full whitespace-wrap" width={80} alt="NextUI hero Image" src={`/me.png`} />
+      <p className="mb-8 max-w-[500px] whitespace-wrap text-slate-400 text-lg text-justify w-full">
+        Even though I&apos;m not actively seeking new opportunities right now, my inbox is always open. If you have a question or just want to say hello, I&apos;ll do my best to respond!
       </p>
 
       <a href="mailto:hey@gonzalofranco.com" target="_blank">
--- src/components/home/rightPannelComponents/cards/projectCard.js
+++ src/components/home/rightPannelComponents/cards/projectCard.js
@@ -53,11 +53,8 @@ const ProjectCard = ({ project }) => {
       <div className={`${styles.content} text-left text-sm flex-1`}>
         <h3 className="text-slate-200 text-lg">
           {title} {' '}
-          <Link href={external} target="_blank" className="transition">
-            {external}
-          </Link>
         </h3>
-        <p className="cursor-default text-slate-200 text-md mb-2">{description}</p>
+        <p className="cursor-default text-slate-200 text-md mb-2 mt-4">{description}</p>
         <div className="flex flex-wrap mt-6">
           {tech &&
             tech.map((skill, index) => (
@@ -66,6 +63,14 @@ const ProjectCard = ({ project }) => {
               </span>
             ))}
         </div>
+        <Link href={external} target="_blank" className="transition mt-2 mb-4 flex flex-row align-center items-center">
+          {github == external ? (
+            <div className="transition mt-2 mb-4 flex flex-row align-center items-center">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="h-4 w-4 mr-4" aria-hidden="true"><path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"></path></svg>
+              <span className="text-green-300">Fork Repo</span>
+            </div>
+          ) : 'View Project'}
+        </Link>
       </div>
     </div>
   );
